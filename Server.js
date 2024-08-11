67e3cc05806095212759b8df727eb576635d9f03
sidaswal7 <siddhantaswal7@gmail.com>
2024-08-11 12:59:05 +0530

    added route to server

--- Server.js
+++ Server.js
@@ -1,8 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-  console.log("Siddhant")
-  process.exit()
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head><title>My First Page</title><body><h1>Welcome to Node Js</h1></body></head>')
+        res.write('</html>')
+        return res.end()
+    } else if(url === '/home'){
+        res.write('<html>')
+        res.write('<head><title>My First Page</title><body><h1>This is the Homepage</h1></body></head>')
+        res.write('</html>')
+        return res.end()
+    }
+    console.log("Siddhant")
+  //process.exit()
 })
 
 server.listen(4000);
