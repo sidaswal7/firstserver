0704b5da8c69a625f3c4b6f98c7672abcdbf007a
sidaswal7 <siddhantaswal7@gmail.com>
2024-08-14 17:46:09 +0530

    addded func to write in file

--- Server.js
+++ Server.js
@@ -1,20 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     const url = req.url;
+    let method = req.method;
     if(url === '/'){
         res.write('<html>')
-        res.write('<head><title>My First Page</title><body><h1>Welcome to Node Js</h1></body></head>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
         res.write('</html>')
         return res.end()
-    } else if(url === '/home'){
-        res.write('<html>')
-        res.write('<head><title>My First Page</title><body><h1>This is the Homepage</h1></body></head>')
-        res.write('</html>')
+    } else if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk)
+            body.push(chunk)
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody)
+            let message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
+        res.statusCode = 302;
+        res.setHeader('Location','/');
         return res.end()
     }
-    console.log("Siddhant")
+    
   //process.exit()
 })
 
-server.listen(4000);
+server.listen(4000, ()=>console.log("server listening on 4000"));
