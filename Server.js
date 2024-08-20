4f0bb5dd8e8cae5b7eb67ba1366203e6d6a0aa6d
sidaswal7 <siddhantaswal7@gmail.com>
2024-08-20 18:19:59 +0530

    added express

--- Server.js
+++ Server.js
@@ -1,6 +1,14 @@
 const http = require('http');
-const routes = require('./routes')
-
-const server = http.createServer(routes)
+const express = require('express')
+const app = express()
+app.use((req,res,next)=>{
+    console.log("inside middleware")
+    next()
+})
+app.use((req,res,next)=>{
+    console.log("inside second middleware")
+    res.send('<h1>Hello Express</h1>')
+})
+const server = http.createServer(app)
 
 server.listen(4000, ()=>console.log("server listening 4000"));
